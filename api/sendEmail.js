387a92abde08c4c494ea0557c640d5a9e269c228
mcerjak <mcerjak@85.cerfrance.fr>
2024-04-15 13:11:10 +0200

    log sendEmail

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -18,24 +18,24 @@ const sendEmail = async (req, res) => {
         to: process.env.USER_EMAIL,
         subject: 'Nouvelle demande de réservation',
         text: `Détails de la réservation:
-               Nom: ${nom}
-               Prénom: ${prenom}
-               Email: ${email}
-               Téléphone: ${telephone}
-               Date d'arrivée: ${dateArrivee}
-               Date de départ: ${dateDepart}
-               Heure de départ: ${heureDepart}
-               Nombre de personnes: ${nbPersonnes}
-               Commentaires: ${commentaires}`
-      };
-      
+                Nom: ${nom}
+                Prénom: ${prenom}
+                Email: ${email}
+                Téléphone: ${telephone}
+                Date d'arrivée: ${dateArrivee}
+                Date de départ: ${dateDepart}
+                Heure de départ: ${heureDepart}
+                Nombre de personnes: ${nbPersonnes}
+                Commentaires: ${commentaires}`
+    };
+
 
     try {
         await transporter.sendMail(mailOptions);
         res.status(200).send('Email sent successfully');
     } catch (error) {
         console.error('Failed to send email', error);
-        res.status(500).send('Failed to send email');
+        res.status(500).send(`Failed to send email for ${process.env.USER_EMAIL}: ${error.message}`);
     }
 };
 
